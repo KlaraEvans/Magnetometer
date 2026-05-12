47d03156dd334fdfd9fbe17913a1ef21b940c27a
oleksandra <forgermany24@gmail.com>
2026-05-12 06:59:13 +0200

    Einstellungen für den anderen Sensor hinzufügt

--- esp32/cos_generator/cos_generator.ino
+++ esp32/cos_generator/cos_generator.ino
@@ -1,15 +1,16 @@
 #include <DacESP32.h>
 DacESP32 dac1(GPIO_NUM_25);
-const int LED = 2;
-//const int sign = 23;
+const int LED = 23;
 void setup() {
   // put your setup code here, to run once:
-   dac1.outputCW(4100); 
+   //dac1.outputCW(4100); //1 Blatt Sensor
+  dac1.outputCW(4260); //4 Blatt Sensor
    pinMode(LED, OUTPUT);
-   //pinMode(23, INPUT);
    digitalWrite(LED, HIGH);
 }
 
 void loop() {
-      dac1.setCwScale(DAC_CW_SCALE_1); 
+      //dac1.setCwScale(DAC_CW_SCALE_1); // 1 blatt Sensor 
+      dac1.setCwScale(DAC_CW_SCALE_8);
+
 }
